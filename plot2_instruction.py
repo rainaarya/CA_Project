4990982cc08c49a59f5d9eea5048b5545712a611
Shivansh Mittal <88429611+Shivansh20128@users.noreply.github.com>
2022-12-20 01:27:39 +0530

    Create plot2_instruction.py
    added instruction plot 2

---
+++ plot2_instruction.py
@@ -0,0 +1,24 @@
+from bitarray.util import ba2int
+import matplotlib.pyplot as plt
+import bitarray
+
+file1 = open('snapshot.txt', 'r')
+Lines = file1.readlines()
+yaxis=[]
+xaxis=[]
+count = 0
+ins=1
+for line in Lines:
+    if line.startswith("cycle"):
+        ins=line[6:-2]
+        xaxis.append(ins)
+
+for line in Lines:
+    if line.startswith("IF.PC"):
+        mem=line[6:39]
+        bit = bitarray.bitarray(mem)
+        bit=ba2int(bit)
+        yaxis.append(bit)
+    else:
+        continue;
+plt.plot(xaxis,yaxis,'ro')
